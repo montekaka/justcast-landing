f5c8ea776060631c4f59c821d2715eda7a9e743d
jia <jia@slyng.com>
2019-12-16 00:11:59 -0800

    removes hydrate

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 // import ReactDOM from 'react-dom';
-import { hydrate, render } from "react-dom";
+// import { hydrate, render } from "react-dom";
 import { Router } from "react-router";
 import { createBrowserHistory } from "history";
 import App from './App';
@@ -11,16 +11,20 @@ import './assets/stylesheets/custom.css'
 import './assets/css/theme.css'
 import './assets/fonts/Feather/feather.css'
 
-// ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 const history = createBrowserHistory();
 
-const rootElement = document.getElementById("root");
+ReactDOM.render(
+  <Router history={history}><App /></Router>
+  , document.getElementById('root'));
 
-if (rootElement.hasChildNodes()) {
-  hydrate(<Router history={history}><App /></Router>, rootElement);
-} else {
-  render(<Router history={history}><App /></Router>, rootElement);
-}
+// const rootElement = document.getElementById("root");
+
+// if (rootElement.hasChildNodes()) {
+//   hydrate(<Router history={history}><App /></Router>, rootElement);
+// } else {
+//   render(<Router history={history}><App /></Router>, rootElement);
+// }
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
