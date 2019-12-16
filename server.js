a8615a910a525612c6eeda0aef12b6d59a3d4330
Josh Chen <josh.chen.jia@gmail.com>
2019-12-16 10:34:29 -0800

    adds og tag for SEO

--- server.js
+++ server.js
@@ -2,6 +2,23 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
 const path = require('path');
+const fs = require('fs')
+
+
+app.get('/shows/:id/audioposts', function(request, response) {
+  console.log('Home page visited!');
+  const filePath = path.resolve(__dirname, './build', 'index.html');
+  fs.readFile(filePath, 'utf8', function (err,data) {
+    if (err) {
+      return console.log(err);
+    }
+    data = data.replace(/\$OG_TITLE/g, 'Home Page');
+    result = data.replace(/\$OG_DESCRIPTION/g, "Home page description");
+    //result = data.replace(/\$OG_IMAGE/g, 'https://i.imgur.com/V7irMl8.png');
+    response.send(result);
+  });
+});
+
 
 app.use(express.static(path.resolve(__dirname, './build')));
 
