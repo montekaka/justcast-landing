a14592dbf1ea4ce94b672a8b38952dcbbeeb70ec
Josh Chen <josh.chen.jia@gmail.com>
2019-12-18 11:18:39 -0800

    adds WidgetPlayer component

--- src/components/players/WidgetPlayer.js
+++ src/components/players/WidgetPlayer.js
@@ -1,56 +1,21 @@
 import React from "react";
 import ReactPlayer from 'react-player'
-import moment from 'moment'
-
-var momentDurationFormatSetup = require("moment-duration-format");
+import FullPlayer from './FullPlayer'
 
 const WidgetPlayer = ({minimize, audio_date, artwork, name, url, duration, playedSeconds, played, playing, handleDuration, handleProgress, handlPlayPauseClick, handleSliderChange, handleSeekMouseDown, handleSeekMouseUp, handlePlayerRef, handleMinimizePlayer}) => {
   return (
-    <div className="widget-player-container dark-html-widget-player">
-      <section className="widget-player-app">
-        <div className="artwork">
-          <img src="https://images.transistor.fm/file/transistor/images/show/15/medium_1521237846-artwork.jpg" />
-        </div>
-        <div className="main">
-          <section className="podcast-name">
-              This is a long string that is OK to truncate please and thank you
-          </section>
-          <section className="episode-name">
-              This is a long string that is OK to truncate please and thank you
-          </section>
-          <section className="controls">
-            <div className="play-button">
-              Play / Pause
-            </div>
-            <div className="miscellaneous">
-              <div className="player-progress-bar">
-                <input type="range" className="input-progress"/>
-                <progress max="100" value="80" />
-              </div>          
-              <div className="buttons">            
-                <section className="time">
-                  <span>04:12</span>
-                  <span>|</span>
-                  <span>10:22</span>              
-                </section>
-              </div>
-            </div>            
-          </section>
-          <section className="menu">
-            <nav>
-              <a className="item">subscribe</a>
-              <a className="item">SHARE</a>
-              <a className="item">MORE INFO</a>
-            </nav>
-            <div className="power-by">
-            </div>
-          </section>
-        </div>
-        <div className="minimize-button">
-          M
-        </div>
-      </section>
-    </div>    
+    <>
+      <FullPlayer progressBarIdName="widgtet-player-progressbar" minimize={minimize} audio_date={audio_date} artwork={artwork} name={name} url={url} duration={duration} playedSeconds={playedSeconds} played={played} playing={playing} handleDuration={handleDuration} handleProgress={handleProgress} handlPlayPauseClick={handlPlayPauseClick} handleSliderChange={handleSliderChange} handleSeekMouseDown={handleSeekMouseDown} handleSeekMouseUp={handleSeekMouseUp} handlePlayerRef={handlePlayerRef}/>
+      <ReactPlayer url={url}
+          className='react-player'
+          width='0%'
+          height='0%'
+          ref={handlePlayerRef}
+          onDuration={handleDuration}
+          onProgress={handleProgress}
+          playing={playing}
+        />      
+    </>
   )
 }
 
