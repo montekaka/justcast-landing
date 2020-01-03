a8ebb5cd44a7ce53cf0cc6d2ea6e2a639babf906
jia <jia@slyng.com>
2020-01-03 00:26:57 -0800

    adds video

--- src/components/LandingPageAbout.js
+++ src/components/LandingPageAbout.js
@@ -6,31 +6,28 @@ const LandingPageAbout = () => {
       <div className="container">
         <div className="row align-items-center">
           <div className="col-12 col-md-7 col-lg-6" data-aos="fade-right">
-            <h2> We have lots of experience building Bootstrap themes</h2>
-            <p className="font-size-lg text-muted mb-6">We've built well over a dozen Bootstrap themes and sold tens of thousands of copies.</p>
-
+            <h2>The easiest way to start podcasting</h2>          
             <div className="d-flex">
               <div className="ml-5">
-                <h4 className="mb-1">Bootstrap users since the beginning</h4>
-                <p className="text-muted mb-6">We've been developing with Bootstrap since it was publicly released in 2011.</p>
+                <h4 className="mb-1">1. Create a Dropbox folder for your show</h4>
+                <p className="text-muted mb-6">Create a new show is as easy as create a new folder inside your dropbox.</p>
               </div>
             </div>
-
             <div className="d-flex">
               <div className="ml-5">
-                <h4 className="mb-1">Bootstrap users since the beginning</h4>
-                <p className="text-muted mb-6">We've been developing with Bootstrap since it was publicly released in 2011.</p>
+                <h4 className="mb-1">2. Save your audio files in that folder</h4>
+                <p className="text-muted mb-6">To add a new epsiode, just drag the mp3 file to your Dropbox folder.</p>
               </div>
             </div>            
-
           </div>
+
           <div className="col-12 col-md-5 col-lg-6">            
             <div className="w-md-150 w-lg-130 position-relative" data-aos="fade-left">
-              <div className="img-skewed img-skewed-left">
-                <img src="https://images.unsplash.com/photo-1556155092-490a1ba16284?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80" className="screenshot img-fluid img-skewed-item" alt="..."/>
+              <div className="embed-responsive embed-responsive-16by9">
+                <iframe className="embed-responsive-item" src="https://player.vimeo.com/video/76843050?title=0&byline=0&portrait=0&api=1&player_id=pPlayer" allowfullscreen></iframe>
               </div>
             </div>
-          </div>
+          </div>                    
         </div>
       </div>
     </section>
