adc1138882f86120053175ca4e54fe5538687482
Josh Chen <josh.chen.jia@gmail.com>
2020-01-07 14:49:34 -0800

    adds mixpanel as provider to the app

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
 import React from 'react';
+import mixpanel from 'mixpanel-browser';
+import { MixpanelProvider, MixpanelConsumer } from 'react-mixpanel'
 import {Provider as PodcastProvider} from './context/PodcastContext'
 import {Provider as PlayerProvider} from './context/PlayerContext'
 import MainRoutes from './components/MainRoutes'
 
+mixpanel.init("YOUR_TOKEN");
+
 function App() {
   return (
-    <PodcastProvider>
-      <PlayerProvider>
-        <MainRoutes/>
-      </PlayerProvider>      
-    </PodcastProvider>    
+    <MixpanelProvider mixpanel={mixpanel}>
+      <PodcastProvider>
+        <PlayerProvider>
+          <MainRoutes/>
+        </PlayerProvider>      
+      </PodcastProvider>    
+    </MixpanelProvider>
   );
 }
 
