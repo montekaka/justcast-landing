c241b951252aac2587b17321051f47486031ee4d
jia <jia@slyng.com>
2020-03-15 23:22:38 -0700

    adds crisp chat to landing page route

--- src/components/LandingPageRoute.js
+++ src/components/LandingPageRoute.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useEffect} from "react";
 import { Route } from 'react-router-dom'
 import LandingPageNavbars from './LandingPageNavbars'
 import LandingPageFooter from './LandingPageFooter'
@@ -8,6 +8,23 @@ const navItems = [
   {label:'About us', url: "/about_us"}
 ]
 const LandingPageRoute = ({ component: Component, ...rest }) => {
+  useEffect(() => {
+    console.log('hi')
+    // Include the Crisp code here, without the <script></script> tags
+    window.$crisp = [];
+    window.CRISP_WEBSITE_ID = process.env.REACT_APP_CRISP_CHAT_WEBSITE_ID;
+
+    (function() {
+      var d = document;
+      var s = d.createElement("script");
+
+      s.src = "https://client.crisp.chat/l.js";
+      s.async = 1;
+      d.getElementsByTagName("head")[0].appendChild(s);
+    })();
+
+  }, [])
+
   return (
     <Route {...rest} render={(props) => (
       <>
