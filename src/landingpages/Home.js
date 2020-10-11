e9f8165a924897490e600639eb206b5e53cce55d
montekaka <josh.chen.jia@gmail.com>
2020-10-10 18:31:59 -0700

    adds return user tracker

--- src/landingpages/Home.js
+++ src/landingpages/Home.js
@@ -15,25 +15,33 @@ import LandingTwitterIntegration from './../components/landingpages/LandingTwitt
 const Home = (props) => {
   const {state, initLocalStorageState, add} = useContext(LocalStorageContext);
 
-  Mixpanel.track('Landing page loaded', {"First Time": "TRUE"}, () => {
-    setTimeout(Mixpanel.register({"First Time": "FALSE"}), 500);
-  });
+  // console.log(state.first_time)
+  // Mixpanel.track('Landing page loaded', {"First Time": "TRUE"}, () => {
+  //   setTimeout(Mixpanel.register({"First Time": "FALSE"}), 500);
+  // });
 
   // google ads url example
   // https://www.justcast.com/?utm_campaign=172194957&utm_source=google&utm_medium=cpc&utm_content=433113468884&utm_term=making%20podcasts&adgroupid=9866620317&gclid=EAIaIQobChMI77y4p6bT6gIVsCCtBh1iCQPgEAAYBCAAEgLlGPD_BwE
 
   useEffect(() => {
-    const values = queryString.parse(props.location.search);
-    if(values && values["via"]) {
-      const via = values['via'];
-      initLocalStorageState({via})
-    } else if (values && values["utm_term"]) {
-      const utm_term = values['utm_term'];
-      initLocalStorageState({utm_term})      
+    const item = localStorageManagement.getAll();
+    if (item.return_user) { 
+      Mixpanel.track('Landing page loaded', {"Return user": "true"});
+    } else {
+      Mixpanel.track('Landing page loaded', {"Return user": "false"});       
+      const values = queryString.parse(props.location.search);
+      if(values && values["via"]) {
+        const via = values['via'];
+        initLocalStorageState({via, return_user: true})
+      } else if (values && values["utm_term"]) {
+        const utm_term = values['utm_term'];
+        initLocalStorageState({utm_term, return_user: true})      
+      }
+      else {
+        initLocalStorageState({return_user: true})
+      }
     }
-    else {
-      initLocalStorageState({})
-    }
+
   }, []);
 
   return (
