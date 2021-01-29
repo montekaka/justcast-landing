0b23a0eee7d070c2361a9dfb096a36267c3ed719
montekaka <josh.chen.jia@gmail.com>
2021-01-28 20:37:49 -0800

    adds including the account_id to the api call

--- src/podcastpages/Tipjar.js
+++ src/podcastpages/Tipjar.js
@@ -10,19 +10,24 @@ import PrivateShow from './../components/PrivateShow';
 
 // Make sure to call `loadStripe` outside of a component’s render to avoid
 // recreating the `Stripe` object on every render.
-const stripePromise = loadStripe(process.env.REACT_APP_STRIPE_PUBLIC_KEY);
+// const stripePromise = loadStripe(process.env.REACT_APP_STRIPE_PUBLIC_KEY);
 
 const Tipjar = (props) => {
   const { state } = useContext(PublicPodcastContext);  
   const id = props.match.params.id;
   const [prices, setPrices] = useState([]);
+  const [accountId, setAccountId] = useState('');
   
+  // const stripePromise = loadStripe('pk_test_TYooMQauvdEDq54NiTphI7jx');
+  // let stripePromise;
   const _ = useShowQuery(id);  
 
   useEffect(() => {
     justcastApi.get((`/v1/shows/${id}/tip_jar_prices_public`))
     .then((res) => {
-      setPrices(res.data);
+      setAccountId(res.data.stripe_user_id)
+      setPrices(res.data.prices);
+      // setPrices(res.data);
     })
     .catch((err) => {
       console.log(err);
@@ -30,30 +35,41 @@ const Tipjar = (props) => {
   }, [id])  
 
   const handlePriceClick = (price_id) => {
-    console.log({price_id})
-    // justcastApi.post(`/v1/create-checkout-session/${price_id}`)
-    // .then((res) => {
-    //   return res.data;
-    // })
-    // .then((session) => {
-    //   return stripePromise.redirectToCheckout({
-    //     sessionId: session.id
-    //   })
-    // })
-    // .then((res) => {
-    //   if(res.error) {
-    //     console.log(res.error.message);
-    //     // If `redirectToCheckout` fails due to a browser or network
-    //     // error, display the localized error message to your customer
-    //     // using `result.error.message`.         
-    //   }
-    // })
-    // .catch((err) => {
-    //   console.log(err);
-    //   // If `redirectToCheckout` fails due to a browser or network
-    //   // error, display the localized error message to your customer
-    //   // using `result.error.message`.      
-    // })    
+    // console.log({price_id})
+    let sessionId = '';
+
+    justcastApi.post(`/v1/create-checkout-session/${price_id}`, {
+      callback_url: `${process.env.REACT_APP_BASE_PATH}/shows/${id}/support_us`
+    })
+    .then((res) => {
+      return res.data;
+    })
+    .then((session) => {
+      sessionId = session.id;
+      return loadStripe(process.env['REACT_APP_STRIPE_PUBLIC_KEY'], {
+        stripeAccount: accountId
+      });
+          
+    })
+    .then((stripe) => {
+      return stripe.redirectToCheckout({
+        sessionId: sessionId
+      })
+    })
+    .then((res) => {
+      if(res.error) {
+        console.log(res.error.message);
+        // If `redirectToCheckout` fails due to a browser or network
+        // error, display the localized error message to your customer
+        // using `result.error.message`.         
+      }
+    })
+    .catch((err) => {
+      console.log(err);
+      // If `redirectToCheckout` fails due to a browser or network
+      // error, display the localized error message to your customer
+      // using `result.error.message`.      
+    })    
   }
   
   if(state.is_private) {
