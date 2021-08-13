46719395e28047107b1eec2562f3c008774d156d
montekaka <josh.chen.jia@gmail.com>
2021-08-12 22:30:13 -0700

    changes to use ninja player for the playlist player

--- src/widgets/WidgetPlaylist.js
+++ src/widgets/WidgetPlaylist.js
@@ -1,6 +1,7 @@
 import React, {useEffect, useState} from "react";
 import { Spinner } from 'reactstrap';
 import ReactGA from 'react-ga';
+import {NinjaPlayer} from 'react-podcast-ninja'
 import justcastApi from '../api/justcast'
 import WidgetPlayerControl from './WidgetPlayerControl';
 import WidgetPlaylistItem from './WidgetPlaylistItem'
@@ -12,6 +13,8 @@ const WidgetPlaylist = (props) => {
   const [selectedAudiopost, setSelectedAudiopost] = useState({});
   const [autoplay, setAutoplay] = useState(false);
   const [menuItems, setMenuItems] = useState([])
+  const [playerConfigs, setPlayerConfigs] = useState({});
+  const [episodes, setEpisodes] = useState([])
 
   useEffect(() => {
     const referer_url = document.referrer;
@@ -37,7 +40,49 @@ const WidgetPlaylist = (props) => {
       if(googleAnalyticsId) {
         ReactGA.initialize(googleAnalyticsId);
         ReactGA.pageview(`/widget/${res.data.show.slug}/audioposts`)
-      }      
+      }   
+      
+
+      // ninja player variables
+      const items = [];
+      const feedArtwork = res.data.show.artwork_url;
+      const podcastTitle = res.data.show.name;
+      
+      res.data.audioposts.forEach((item) => {
+        let artworkUrl = feedArtwork;
+        if(item.artwork_url) {
+          artworkUrl = item.artwork_url;
+        }
+
+        const chaptersUrl = item.chapters_url
+        // console.log(artworkUrl)
+        items.push({
+          title: item.name,
+          description: item.description,
+          podcastTitle: podcastTitle,
+          artworkUrl: artworkUrl,
+          pubDate: item.audio_date,
+          link: item.single_page_url,
+          audioUrl: item.url,
+          chaptersUrl: chaptersUrl,
+        })
+      })
+      
+      setEpisodes(items);    
+      
+      const configs = {
+        hidePubDate: res.data.show.hide_widget_pub_date,
+        primaryBackgroundColor: res.data.show.widget_primary_background_color ?  res.data.show.widget_primary_background_color : "#0c1824",
+        primaryButtonColor: res.data.show.widget_primary_button_color ?  res.data.show.widget_primary_button_color : "#f7f8f9",
+        primaryTextColor: res.data.show.widget_primary_text_color ?  res.data.show.widget_primary_text_color : "#f7f8f9",
+        progressBarFilledColor: res.data.show.widget_progress_bar_filled_color ?  res.data.show.widget_progress_bar_filled_color : "#f7f8f9",
+        progressBarBackgroundColor: res.data.show.widget_progress_bar_background_color ?  res.data.show.widget_progress_bar_background_color : "#8A8175",
+        playlistBackgroundColor: res.data.show.widget_playlist_background_color ?  res.data.show.widget_playlist_background_color : "#30343c",
+        playlistTextColor: res.data.show.widget_playlist_text_color ?  res.data.show.widget_playlist_text_color : "#f7f8f9",
+        chapterBackgroundColor: res.data.show.widget_chapter_background_color ?  res.data.show.widget_chapter_background_color : "#30343c",
+        chapterTextColor:  res.data.show.widget_chapter_text_color ?  res.data.show.widget_chapter_text_color : "#f7f8f9"
+      }
+      setPlayerConfigs(configs)      
     })
     .catch((err) => {
       console.log('this is not available')
@@ -54,39 +99,49 @@ const WidgetPlaylist = (props) => {
 
   if(show.id) {
     return (
-      <>
-        <WidgetPlayerControl
-          playerControlSquare={true}
-          id={selectedAudiopost.id}
-          showId={id}
-          show={show}
-          audiopostData={selectedAudiopost}
-          autoplay={autoplay}
-          menuItems={menuItems}
-        />    
-        <div className="widget-playlist">
-          <div className="playlist-header">
-            <div>{audioposts.length} LATEST EPISODES</div>
-          </div>
-          <div className="playlist-items">
-            {
-              audioposts.map((audiopost) => 
-                <WidgetPlaylistItem 
-                  key={audiopost.id.toString()} 
-                  name={audiopost.name} 
-                  id={audiopost.id} 
-                  audioDate={audiopost.audio_date}
-                  duration={audiopost.duration}
-                  selectedId={selectedAudiopost.id}
-                  handleClicked={handleAudiopostClicked}
-                />            
-              )
-            }
-          </div>
-        </div>
-      </>
-    )
+      <NinjaPlayer
+        configs={playerConfigs}
+        playerId={`${id}-playlist`}
+        episodes={episodes}
+      />  
+    )  
   }
+
+  // if(show.id) {
+  //   return (
+  //     <>
+  //       <WidgetPlayerControl
+  //         playerControlSquare={true}
+  //         id={selectedAudiopost.id}
+  //         showId={id}
+  //         show={show}
+  //         audiopostData={selectedAudiopost}
+  //         autoplay={autoplay}
+  //         menuItems={menuItems}
+  //       />    
+  //       <div className="widget-playlist">
+  //         <div className="playlist-header">
+  //           <div>{audioposts.length} LATEST EPISODES</div>
+  //         </div>
+  //         <div className="playlist-items">
+  //           {
+  //             audioposts.map((audiopost) => 
+  //               <WidgetPlaylistItem 
+  //                 key={audiopost.id.toString()} 
+  //                 name={audiopost.name} 
+  //                 id={audiopost.id} 
+  //                 audioDate={audiopost.audio_date}
+  //                 duration={audiopost.duration}
+  //                 selectedId={selectedAudiopost.id}
+  //                 handleClicked={handleAudiopostClicked}
+  //               />            
+  //             )
+  //           }
+  //         </div>
+  //       </div>
+  //     </>
+  //   )
+  // }
   return <Spinner color="primary" />
 }
 
