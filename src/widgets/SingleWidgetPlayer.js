f91992fbbbdf6c4d89b887158ec274fa1c4a1e67
montekaka <josh.chen.jia@gmail.com>
2021-08-17 20:13:37 -0700

    adds ninja player to widget

--- src/widgets/SingleWidgetPlayer.js
+++ src/widgets/SingleWidgetPlayer.js
@@ -1,18 +1,17 @@
 import React, {useEffect, useState} from "react";
-import axios from 'axios'
 import ReactGA from 'react-ga';
 import justcastApi from '../api/justcast'
-import WidgetPlayerControl from './WidgetPlayerControl';
+import {NinjaPlayer} from 'react-podcast-ninja'
 
 const SingleWidgetPlayer = (props) => {
   const id = props.match.params.id;
   const showId = props.match.params.show_id;  
   const [audiopost, setAudiopost] = useState({});
+  const [episodes, setEpisodes] = useState([])
+  const [playerConfigs, setPlayerConfigs] = useState({});
   const [show, setShow] = useState({});
   const [menuItems, setMenuItems] = useState([])
-  const [chaptersUrl, setChaptersUrl] = useState('');
-  const [chapters, setChapters] = useState([]);
-
+  
   useEffect(() => {
     const referer_url = document.referrer;
     justcastApi.get(`/v1/shows/${showId}/audioposts/${id}?referer_url=${referer_url}`)
@@ -37,42 +36,62 @@ const SingleWidgetPlayer = (props) => {
         ReactGA.pageview(`/widget/${res.data.show.slug}/audioposts/${id}`)
       }
 
-      if(data.chapters_url && data.chapters_url.length > 10) {
-        setChaptersUrl(data.chapters_url)
+      // ninja player variables
+      const items = [];
+      const feedArtwork = data.show.artwork_url;
+      const podcastTitle = data.show.name;
+      
+      let artworkUrl = feedArtwork;
+      if(data.artwork_url) {
+        artworkUrl = data.artwork_url;
       }
+
+      const chaptersUrl = data.chapters_url
+      // console.log(artworkUrl)
+      const item = {
+        title: data.name,
+        description: data.description,
+        podcastTitle: podcastTitle,
+        artworkUrl: artworkUrl,
+        pubDate: data.audio_date,
+        link: data.single_page_url,
+        audioUrl: data.url,
+        chaptersUrl: chaptersUrl,
+      }
+      
+      
+      setEpisodes([item]);    
+      
+      const configs = {
+        hidePubDate: res.data.show.hide_widget_pub_date,
+        primaryBackgroundColor: res.data.show.widget_primary_background_color ?  res.data.show.widget_primary_background_color : "#0c1824",
+        primaryButtonColor: res.data.show.widget_primary_button_color ?  res.data.show.widget_primary_button_color : "#f7f8f9",
+        primaryTextColor: res.data.show.widget_primary_text_color ?  res.data.show.widget_primary_text_color : "#f7f8f9",
+        progressBarFilledColor: res.data.show.widget_progress_bar_filled_color ?  res.data.show.widget_progress_bar_filled_color : "#f7f8f9",
+        progressBarBackgroundColor: res.data.show.widget_progress_bar_background_color ?  res.data.show.widget_progress_bar_background_color : "#8A8175",
+        playlistBackgroundColor: res.data.show.widget_playlist_background_color ?  res.data.show.widget_playlist_background_color : "#30343c",
+        playlistTextColor: res.data.show.widget_playlist_text_color ?  res.data.show.widget_playlist_text_color : "#f7f8f9",
+        chapterBackgroundColor: res.data.show.widget_chapter_background_color ?  res.data.show.widget_chapter_background_color : "#30343c",
+        chapterTextColor:  res.data.show.widget_chapter_text_color ?  res.data.show.widget_chapter_text_color : "#f7f8f9"
+      }
+      setPlayerConfigs(configs)  
+
     })
     .catch((err) => {
       console.log(err);
     })
   }, [showId, id])  
 
-  useEffect(() => {
-    if(chaptersUrl && chaptersUrl.length > 10) {
-      axios.get(chaptersUrl)
-      .then((res) => {
-        if(res.data && res.data.chapters && res.data.chapters.length > 0) {
-          setChapters(res.data.chapters);                      
-        }
-      })
-      .catch((err) => {
-
-      })
-    }
-  }, [chaptersUrl])
-
-  if(audiopost.id) {
+  if(episodes.length > 0) {
     return (
-      <>
-        <WidgetPlayerControl
-          id={id}
-          showId={showId}
-          show={show}
-          playerControlSquare={true}
-          audiopostData={audiopost}
-          menuItems={menuItems}
-          chapters={chapters}
-        />
-      </>
+      <div style={{height: "100vh", width: "100%", background: "#d0dee9", margin: 0, padding: 0, boxSizing: 'border-box'}}>
+        <NinjaPlayer
+          configs={playerConfigs}
+          playerId={`${id}-single`}
+          episodes={episodes}
+          singleEpisode={true}
+        />  
+      </div>
     )
   }
   return null;
