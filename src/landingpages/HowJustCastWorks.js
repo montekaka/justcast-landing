ab5ee6ae80ffda9293685b6a3f75306d9686fe5f
montekaka <josh.chen.jia@gmail.com>
2021-08-20 21:50:42 -0700

    adds video on how to start a podcast with justcast

--- src/landingpages/HowJustCastWorks.js
+++ src/landingpages/HowJustCastWorks.js
@@ -20,6 +20,18 @@ const HowJustCastWorks = () => {
         <div className="container">
           <div className="row justify-content-center">
             <div className="col-12 col-md-10 col-lg-9 col-xl-8">
+              <p>Here are a simple instructions on how to host your podcast with JustCast</p>
+              <div className="embed-responsive embed-responsive-16by9">
+                <iframe className="embed-responsive-item" src="https://player.vimeo.com/video/590140243" allowFullScreen></iframe>	
+              </div>              
+            </div>
+          </div>
+        </div>
+      </section>      
+      <section className="pt-6 pt-md-8">
+        <div className="container">
+          <div className="row justify-content-center">
+            <div className="col-12 col-md-10 col-lg-9 col-xl-8">
               <p>Here are a simple instructions on how to host your podcast from Dropbox with JustCast</p>
               <div className="embed-responsive embed-responsive-16by9">
                 <iframe className="embed-responsive-item" src="https://player.vimeo.com/video/414646004" allowFullScreen></iframe>	
