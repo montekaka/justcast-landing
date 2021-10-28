80ccbdb5229f88f9ecfdba494d32731f6db98e96
montekaka <josh.chen.jia@gmail.com>
2021-10-27 17:42:41 -0700

    fix the apple pay file

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ const filePath = path.resolve(__dirname, './build', 'index.html');
 const privatePodcastFilePath = path.resolve(__dirname, './build', 'privatepodcast.html');
 
 const appleVerification = {
-  root: path.join(__dirname, `./build`, 'apple-developer-merchantid-domain-association'),
+  root: path.join(__dirname, `./public/.well-known`),
 };
 
 // apple-developer-merchantid-domain-association
