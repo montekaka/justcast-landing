9bcd432934888693288a8ee1488f5982f3b66ec4
montekaka <josh.chen.jia@gmail.com>
2022-01-27 23:10:16 -0800

    adds color transparent to the people card background

--- src/components/podcastpages/HostCard.js
+++ src/components/podcastpages/HostCard.js
@@ -8,7 +8,7 @@ export default function HostCard(props) {
 // bg-gray-80
   return (
     <div className="host-card" style={{backgroundColor: cardBackgroundColor, color: textColor}}>
-      {img ? <img className="avatar" src={img} alt={`${name} avatar`} /> : <div className="avatar avatar-flex" style={{backgroundColor: buttonColor}}>< div className="avatar-missing-img" style={{backgroundColor: cardBackgroundColor, color: textColor}}>{name.slice(0, 2)}</div></div>}
+      {img ? <img className="avatar" src={img} alt={`${name} avatar`} /> : <div className="avatar avatar-flex" style={{backgroundColor: `${buttonColor}90`}}>< div className="avatar-missing-img" style={{backgroundColor: cardBackgroundColor, color: textColor}}>{name.slice(0, 2)}</div></div>}
       
       {name}      
     </div>
