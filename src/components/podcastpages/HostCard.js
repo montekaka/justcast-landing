8e88975a02ec267f8032b9b77e93981deccb023d
montekaka <josh.chen.jia@gmail.com>
2022-01-29 00:31:10 -0800

    adds toggle condition on host card to read the whole description

--- src/components/podcastpages/HostCard.js
+++ src/components/podcastpages/HostCard.js
@@ -1,4 +1,4 @@
-import React, {useContext} from 'react';
+import React, {useContext, useState} from 'react';
 import {Context as PublicPodcastContext} from '../../context/PublicPodcastContext'
 import { Badge, Button } from 'reactstrap';
 
@@ -6,9 +6,19 @@ export default function HostCard(props) {
   const {img, href, group, role, name, description} = props;
   const { state } = useContext(PublicPodcastContext);
   const {cardBackgroundColor, textColor, buttonColor, buttonTextColor} = state;
+
+  const [cardHeight, setCardHeight] = useState("500px");
+
+  const toggle = () => {
+    if(cardHeight === "500px") {
+      setCardHeight("100%")
+    } else {
+      setCardHeight("500px")
+    }
+  }
 // bg-gray-80
   return (
-    <div className="host-card" style={{backgroundColor: cardBackgroundColor, color: textColor}}>
+    <div className="host-card" style={{height: cardHeight, backgroundColor: cardBackgroundColor, color: textColor}}>
       {img ? <img className="avatar" src={img} alt={`${name} avatar`} /> : <div className="avatar avatar-flex" style={{backgroundColor: `${buttonColor}90`}}>< div className="avatar-missing-img" style={{backgroundColor: cardBackgroundColor, color: textColor}}>{name.slice(0, 2)}</div></div>}
       <div className='host-card-body'>
         <h3>{name}</h3>
@@ -17,7 +27,7 @@ export default function HostCard(props) {
           <Badge>{role}</Badge>
         </div>
         {href ? <a className='badge badge-secondary' href={href} target="_blank"><i className='fe fe-home'/> Home page</a> : null}
-        {description ? <div className='description'>{description}</div> : null}
+        {description ? <div className='description' onClick={toggle}>{description}</div> : null}
         
       </div>      
     </div>
