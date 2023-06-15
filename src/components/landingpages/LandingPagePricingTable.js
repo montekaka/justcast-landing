a0b8b72bf0987e5adc2aceeb81b5ac1ea19aa9ee
montekaka <josh.chen.jia@gmail.com>
2023-06-14 21:19:38 -0700

    fix typo

--- src/components/landingpages/LandingPagePricingTable.js
+++ src/components/landingpages/LandingPagePricingTable.js
@@ -12,7 +12,7 @@ const LandingPagePricingTable = ({sectionClassName, titleColor, hidePersonal}) =
     'Unlimited episodes',
     'Unlimited listeners',
     'Automatic video to audio conversion',
-    'Zapier Integration',
+    'Zapier Steps',
     'Custom embedded player',
     'One-Click listener access',
     'Listener management',
